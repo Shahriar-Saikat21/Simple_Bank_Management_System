c72b5be90dbf64f690d8f093828d0b73610df5e0
Shahriar Saikat <saikatbass@gmail.com>
2021-10-17 00:29:20 +0600

    all func done

--- projrct.c
+++ projrct.c
@@ -134,17 +134,294 @@ void displayAllAccount()
 
 void updateAccount()
 {
-    printf("Done");
+    FILE *fileOne = fopen("accountInfo.bin", "rb");
+    FILE *temp = fopen("temp.bin", "wb");
+
+    Account accountInformation, tempInformation;
+
+    int choice, flag = 0;
+
+    if (fileOne == NULL || temp == NULL)
+    {
+        printf("\n\t\t\tError !\n");
+    }
+
+    system("cls");
+
+    printf("\t\t\t\t====== Update Account Information ======\n");
+
+    printf("\n\t\t\tEnter Account Number : ");
+    getchar();
+    gets(tempInformation.accountNumber);
+
+    while (fread(&accountInformation, sizeof(accountInformation), 1, fileOne) == 1)
+    {
+        if (strcmp(accountInformation.accountNumber, tempInformation.accountNumber) == 0)
+        {
+            flag++;
+            printf("\n\t\t\tChoose your option :\n\t\t\t1.Update Account Name\n\t\t\t2.Update Date of Birth.\n\t\t\t3.Update Address\n\t\t\t4.Update Contact No.");
+            printf("\n\n\t\t\tEnter Your Option : ");
+            scanf("%d", &choice);
+            if (choice == 1)
+            {
+                printf("\n\t\t\tEnter Account's Name to Update: ");
+                getchar();
+                gets(tempInformation.accountName);
+                strcpy(accountInformation.accountName, tempInformation.accountName);
+
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+                printf("\n\n\t\t\tUpdated successfully!\n\n");
+            }
+            else if (choice == 2)
+            {
+                printf("\n\t\t\tEnter Date of Birth to Update: ");
+                getchar();
+                gets(tempInformation.DateOfBirth);
+                strcpy(accountInformation.DateOfBirth, tempInformation.DateOfBirth);
+
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+                printf("\n\n\t\t\tUpdated successfully!\n\n");
+            }
+            else if (choice == 3)
+            {
+                printf("\n\t\t\tEnter Address to Update: ");
+                getchar();
+                gets(tempInformation.address);
+                strcpy(accountInformation.address, tempInformation.address);
+
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+                printf("\n\n\t\t\tUpdated successfully!\n\n");
+            }
+            else
+            {
+                printf("\n\t\t\tInvalid Option.");
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+            }
+        }
+        else
+        {
+            fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+        }
+    }
+
+    fclose(fileOne);
+    fclose(temp);
+
+    remove("accountInfo.bin");
+    rename("temp.bin", "accountInfo.bin");
+
+    if (flag == 0)
+    {
+        printf("\n\t\t\tAccount is not found");
+    }
+
+    printf("\n\n\t\t\tEnter any keys to continue.......");
+    getch();
 }
+
 void deleteAccount()
 {
-    printf("Done");
+    FILE *fileOne = fopen("accountInfo.bin", "rb");
+    FILE *temp = fopen("temp.bin", "wb");
+
+    Account accountInformation, tempInformation;
+
+    int choice, flag = 0;
+
+    if (fileOne == NULL || temp == NULL)
+    {
+        printf("\n\t\t\tError !\n");
+    }
+
+    system("cls");
+
+    printf("\t\t\t\t====== Delete Account Information ======\n");
+
+    printf("\n\t\t\tEnter Account Number : ");
+    getchar();
+    gets(tempInformation.accountNumber);
+
+    while (fread(&accountInformation, sizeof(accountInformation), 1, fileOne) == 1)
+    {
+        if (strcmp(accountInformation.accountNumber, tempInformation.accountNumber) == 0)
+        {
+            flag++;
+            printf("\n\t\t\tAre you sure to delete ??\n\t\t\t\t1.Yes\n\t\t\t\t2.Back\n\t\t\t\tEnter Your Option : ");
+            scanf("%d", &choice);
+            if (choice == 1)
+            {
+                printf("\n\n\t\t\tInformation has been deleted successfully!\n\n");
+            }
+            else if (choice == 2)
+            {
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+            }
+            else
+            {
+                printf("\n\t\t\tInvalid Option");
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+            }
+        }
+        else
+        {
+            fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+        }
+    }
+
+    fclose(fileOne);
+    fclose(temp);
+
+    remove("accountInfo.bin");
+    rename("temp.bin", "accountInfo.bin");
+
+    if (flag == 0)
+    {
+        printf("\n\t\t\tAccount is not found");
+    }
+
+    printf("\n\n\t\t\tEnter any keys to continue.......");
+    getch();
 }
+
 void searchAccount()
 {
-    printf("Done");
+    FILE *fileOne = fopen("accountInfo.bin", "rb");
+
+    Account accountInformation;
+
+    int choice, flag = 0;
+    char searchAccountNumber[20], searchName[50];
+
+    if (fileOne == NULL)
+    {
+        printf("\n\t\t\tError !\n");
+    }
+
+    system("cls");
+
+    printf("\t\t\t\t====== Search Account Information ======\n");
+
+    printf("\n\t\t\tChoose your option :\n\t\t\t1.Search by Account Number\n\t\t\t2.Search by Account's Name");
+    printf("\n\n\t\t\tEnter Your Option : ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        system("cls");
+        printf("\t\t\t\t====== Search Account Information ======\n");
+        printf("\n\n\t\t\tEnter Account Number : ");
+        getchar();
+        gets(searchAccountNumber);
+        while (fread(&accountInformation, sizeof(accountInformation), 1, fileOne) == 1)
+        {
+            if (strcmp(accountInformation.accountNumber, searchAccountNumber) == 0)
+            {
+                flag++;
+                printf("\n\t\t\tAccount Name : %s\n\t\t\tAccount Number : %s\n\t\t\tDate of Birth : %s\n\t\t\tAddress : %s\n\t\t\tContact Number : %s\n\t\t\tCurrent Account's Balance : %.2f\n", accountInformation.accountName, accountInformation.accountNumber, accountInformation.DateOfBirth, accountInformation.address, accountInformation.contactNum, accountInformation.accountBalance);
+            }
+        }
+        if (flag == 0)
+        {
+            printf("\n\t\t\tAccount is not found");
+        }
+    }
+    else if (choice == 2)
+    {
+        system("cls");
+        printf("\t\t\t\t====== Search Account Information ======\n");
+        printf("\n\n\t\t\tEnter Account Name : ");
+        getchar();
+        gets(searchName);
+        while (fread(&accountInformation, sizeof(accountInformation), 1, fileOne) == 1)
+        {
+            if (stricmp(accountInformation.accountName, searchName) == 0)
+            {
+                flag++;
+                printf("\n\t\t\tAccount Name : %s\n\t\t\tAccount Number : %s\n\t\t\tDate of Birth : %s\n\t\t\tAddress : %s\n\t\t\tContact Number : %s\n\t\t\tCurrent Account's Balance : %.2f\n", accountInformation.accountName, accountInformation.accountNumber, accountInformation.DateOfBirth, accountInformation.address, accountInformation.contactNum, accountInformation.accountBalance);
+                printf("\t\t\t---------------------------------------------------\n");
+            }
+        }
+        if (flag == 0)
+        {
+            printf("\n\t\t\tAccount is not found");
+        }
+    }
+    else
+    {
+        printf("\n\t\t\tInvalid Option");
+    }
+
+    fclose(fileOne);
+
+    printf("\n\n\n\t\t\tEnter any keys to continue.......");
+    getch();
 }
+
 void transaction()
 {
-    printf("Done");
+    FILE *fileOne = fopen("accountInfo.bin", "rb");
+    FILE *temp = fopen("temp.bin", "wb");
+
+    Account accountInformation, tempInformation;
+
+    int op, flag = 0;
+
+    if (fileOne == NULL || temp == NULL)
+    {
+        printf("\n\t\t\tError !\n");
+    }
+
+    system("cls");
+
+    printf("\t\t\t\t====== Account Transaction ======\n");
+
+    printf("\n\t\t\tEnter Account Number : ");
+    getchar();
+    gets(tempInformation.accountNumber);
+
+    while (fread(&accountInformation, sizeof(accountInformation), 1, fileOne) == 1)
+    {
+        if (strcmp(accountInformation.accountNumber, tempInformation.accountNumber) == 0)
+        {
+            flag++;
+            printf("\n\n\t\t\tDo you want to\n\t\t\t1.Deposit\n\t\t\t2.Withdraw?\n\n\t\t\tEnter your choice:");
+            scanf("%d", &op);
+            if (op == 1)
+            {
+                printf("\n\t\t\tEnter the amount you want to deposit:$ ");
+                scanf("%f", &tempInformation.accountBalance);
+                accountInformation.accountBalance += tempInformation.accountBalance;
+
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+                printf("\n\n\t\t\tDeposited successfully!\n\n");
+            }
+            else
+            {
+                printf("\n\t\t\tEnter the amount you want to withdraw:$ ");
+                scanf("%f", &tempInformation.accountBalance);
+                accountInformation.accountBalance -= tempInformation.accountBalance;
+
+                fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+                printf("\n\n\t\t\tWithdraw successfully!\n\n");
+            }
+        }
+        else
+        {
+            fwrite(&accountInformation, sizeof(accountInformation), 1, temp);
+        }
+    }
+
+    if (flag == 0)
+    {
+        printf("\n\t\t\tAccount is not found");
+    }
+
+    fclose(fileOne);
+    fclose(temp);
+
+    remove("accountInfo.bin");
+    rename("temp.bin", "accountInfo.bin");
+
+    printf("\n\n\t\t\tEnter any keys to continue.......");
+    getch();
 }
